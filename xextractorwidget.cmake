037a9a2af368ae600ed3e29aceb6b042faf2dd04
horsicq <horsicq@gmail.com>
2022-10-07 17:35:36 +0200

    Fix: 2022-10-07

--- xextractorwidget.cmake
+++ xextractorwidget.cmake
@@ -1,6 +1,9 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 
+include(${CMAKE_CURRENT_LIST_DIR}/../XExtractor/xextractor.cmake)
+
 set(XEXTRACTORWIDGET_SOURCES
+    ${XEXTRACTOR_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/xextractorwidget.cpp
     ${CMAKE_CURRENT_LIST_DIR}/xextractorwidget.ui
     ${CMAKE_CURRENT_LIST_DIR}/dialogxextractor.cpp
