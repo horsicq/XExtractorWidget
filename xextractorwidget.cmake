0d2f5e5d9d077136733dd894d19016b42105f533
horsicq <horsicq@gmail.com>
2024-03-04 00:19:09 +0100

    Update file(s): 2024-03-04

--- xextractorwidget.cmake
+++ xextractorwidget.cmake
@@ -11,8 +11,11 @@ set(XEXTRACTORWIDGET_SOURCES
     ${DIALOGDUMP_SOURCES}
     ${XCOMBOBOXEX_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/xextractorwidget.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xextractorwidget.h
     ${CMAKE_CURRENT_LIST_DIR}/xextractorwidget.ui
     ${CMAKE_CURRENT_LIST_DIR}/dialogxextractor.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/dialogxextractor.h
     ${CMAKE_CURRENT_LIST_DIR}/dialogxextractor.ui
     ${CMAKE_CURRENT_LIST_DIR}/dialogextractorprocess.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/dialogextractorprocess.h
 )
